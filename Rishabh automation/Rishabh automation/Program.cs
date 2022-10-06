fa80f3e4547c4768f2773a850d35c00353155839
Rishabh2730 <bhatiarishabh2730@gmail.com>
2022-10-06 21:53:21 +1300

    first commit

---
+++ Rishabh automation/Rishabh automation/Program.cs
@@ -0,0 +1,44 @@
+﻿//open chrome browser
+
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+IWebDriver driver = new ChromeDriver();
+
+//naviagte to Turnup portal
+
+driver.Navigate().GoToUrl("http://horse.industryconnect.io/Account/Login?ReturnUrl=%2f");
+driver.Manage().Window.Maximize();
+
+//identify username textbox and enter valid username
+
+IWebElement usernameTextbox = driver.FindElement(By.Id("UserName"));
+usernameTextbox.SendKeys("hari");
+   
+
+//identify password textbox and enter valid password
+
+IWebElement passwordTextbox = driver.FindElement(By.Id("Password"));
+passwordTextbox.SendKeys("123123");
+
+//identify login button and click on it
+
+IWebElement Loginbutton = driver.FindElement(By.XPath("//*[@id='loginForm'']/form/div[3]/input[1]"));
+Loginbutton.Click();
+
+//check if user is logged in successfully
+
+//IWebElement 
+
+
+
+
+
+
+
+
+
+
+
+
+
