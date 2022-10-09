edc899fcdd9ab9aab47a7eea29a7bb975c90551d
Rishabh2730 <bhatiarishabh2730@gmail.com>
2022-10-10 02:05:07 +1300

    new material record added

--- Rishabh automation/Rishabh automation/Program.cs
+++ Rishabh automation/Rishabh automation/Program.cs
@@ -2,6 +2,7 @@
 
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using System;
 
 IWebDriver driver = new ChromeDriver();
 
@@ -40,6 +41,82 @@ else
     Console.WriteLine("test failed");
 
 }
+//create material record
+
+
+//navigate to time and material page
+IWebElement administrationDropdown = driver.FindElement(By.XPath("/html/body/div[3]/div/div/ul/li[5]/a/span"));
+administrationDropdown.Click();
+
+IWebElement TimeandMaterial = driver.FindElement(By.XPath("/html/body/div[3]/div/div/ul/li[5]/ul/li[3]/a"));
+TimeandMaterial.Click();
+Thread.Sleep(1000);
+
+
+//Click on "Create new" button
+IWebElement CreateNew = driver.FindElement(By.XPath("//*[@id=\"container\"]/p/a"));
+CreateNew.Click();
+
+//Select material from typecode dropdown
+
+IWebElement TypeCodeDropdown = driver.FindElement(By.XPath("//*[@id=\"TimeMaterialEditForm\"]/div/div[1]/div/span[1]/span/span[2]/span"));
+TypeCodeDropdown.Click();
+Thread.Sleep(1000);
+
+IWebElement MaterialOption = driver.FindElement(By.XPath("//*[@id=\"TypeCode_option_selected\"]"));
+MaterialOption.Click();
+
+//Enter code into code textbox
+IWebElement CodeTextbox = driver.FindElement(By.Id("Code"));
+CodeTextbox.SendKeys("rish");
+
+//Enter the description on description textbox
+
+IWebElement DescriptionTextbox = driver.FindElement(By.Id("Description"));
+DescriptionTextbox.SendKeys("first automation");
+
+//Enter the price per unit in price per unit textbox
+
+IWebElement inputtag = driver.FindElement(By.XPath("//*[@id=\"TimeMaterialEditForm\"]/div/div[4]/div/span[1]/span/input[1]"));
+inputtag.Click();
+
+IWebElement PricePerUnit = driver.FindElement(By.Id("Price"));
+PricePerUnit.SendKeys("27.00");
+
+//click on save button
+
+IWebElement SaveButton = driver.FindElement(By.Id("SaveButton"));
+SaveButton.Click();
+Thread.Sleep(2000);
+
+//check if new material record has been created
+
+IWebElement GoToLastPage = driver.FindElement(By.XPath("//*[@id=\"tmsGrid\"]/div[4]/a[4]/span"));
+GoToLastPage.Click();
+
+IWebElement newCode = driver.FindElement(By.XPath("//*[@id=\"tmsGrid\"]/div[3]/table/tbody/tr[last()]/td[1]"));
+
+
+if (newCode.Text =="rish")
+
+    {
+    Console.WriteLine("Material record created successfully");
+
+}
+else
+{
+    Console.WriteLine("Material record not created");
+
+}
+
+
+
+
+
+
+
+
+
 
 
 
